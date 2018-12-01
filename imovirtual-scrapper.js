c694a31df0fc399a8d492b3142bdf6165f8ae43d
Diogo Teixeira <teixeira.diogo5@gmail.com>
2018-12-01 16:55:12 +0000

    fix

--- imovirtual-scrapper.js
+++ imovirtual-scrapper.js
@@ -13,11 +13,62 @@ const options = {
       $ = cheerio.load(body); 
     },
     headers: {
-        'User-Agent': 'api-imovirtual https://github.com/diogoteix/imovirtual-api',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)' +
+                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
         'Accept-Language': 'pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7,it;q=0.6,es;q=0.5'
     }
   };
+
+const preparePageForTests = async (page) => {
+    // Pass the User-Agent Test.
+    const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' +
+        'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
+    await page.setUserAgent(userAgent);
+
+    // Pass the Webdriver Test.
+    await page.evaluateOnNewDocument(() => {
+        Object.defineProperty(navigator, 'webdriver', {
+        get: () => false,
+        });
+    });
+
+    // Pass the Chrome Test.
+    await page.evaluateOnNewDocument(() => {
+        // We can mock this in as much depth as we need for the test.
+        window.navigator.chrome = {
+            runtime: {},
+            // etc.
+        };
+    });
+
+    // Pass the Permissions Test.
+    await page.evaluateOnNewDocument(() => {
+        const originalQuery = window.navigator.permissions.query;
+        return window.navigator.permissions.query = (parameters) => (
+        parameters.name === 'notifications' ?
+            Promise.resolve({ state: Notification.permission }) :
+            originalQuery(parameters)
+        );
+    });
+
+    // Pass the Plugins Length Test.
+    await page.evaluateOnNewDocument(() => {
+        // Overwrite the `plugins` property to use a custom getter.
+        Object.defineProperty(navigator, 'plugins', {
+        // This just needs to have `length > 0` for the current test,
+        // but we could mock the plugins too if necessary.
+        get: () => [1, 2, 3, 4, 5],
+        });
+    });
+
+    await page.evaluateOnNewDocument(() => {
+        // Overwrite the `plugins` property to use a custom getter.
+        Object.defineProperty(navigator, 'languages', {
+            get: () => ['en-US', 'en'],
+        });
+    });
+}
 
 function getData(client) {
     var data = [];
@@ -28,6 +79,8 @@ function getData(client) {
             args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
         const page = await browser.newPage();
+        // Prepare for the tests (not yet implemented).
+        await preparePageForTests(page);
         await page.goto('https://www.imovirtual.com/comprar/apartamento/vila-nova-de-gaia/?search%5Bfilter_enum_rooms_num%5D%5B0%5D=2&search%5Bfilter_enum_condition%5D%5B0%5D=novo&search%5Bfilter_enum_condition%5D%5B1%5D=em_construcao&search%5Bdescription%5D=1&search%5Bsubregion_id%5D=195&nrAdsPerPage=100');
         let bodyHTML = await page.evaluate(() => document.body.innerHTML);
 
