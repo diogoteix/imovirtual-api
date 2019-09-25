a4fd6a7af898b9a929be8afece82af96b5a20178
Diogo Teixeira <teixeira.diogo5@gmail.com>
2019-09-25 15:46:00 +0100

    drop table

--- imovirtual-scrapper.js
+++ imovirtual-scrapper.js
@@ -1,198 +1,225 @@
-const cheerio = require('cheerio');
+const cheerio = require("cheerio");
 
-var request = require('request');
-
+var request = require("request");
 
 var $;
 
 const options = {
-    uri: `https://www.imovirtual.com/comprar/apartamento/vila-nova-de-gaia/?search%5Bfilter_enum_rooms_num%5D%5B0%5D=2&search%5Bfilter_enum_condition%5D%5B0%5D=novo&search%5Bfilter_enum_condition%5D%5B1%5D=em_construcao&search%5Bdescription%5D=1&search%5Bsubregion_id%5D=195&search%5Border%5D=created_at_first%3Adesc&nrAdsPerPage=100`,
-    transform: function (body) {
-      $ = cheerio.load(body); 
-    },
-    headers: {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)' +
-                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'Accept-Language': 'pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7,it;q=0.6,es;q=0.5'
-    }
-  };
+  uri: `https://www.imovirtual.com/comprar/apartamento/vila-nova-de-gaia/?search%5Bfilter_enum_rooms_num%5D%5B0%5D=2&search%5Bfilter_enum_condition%5D%5B0%5D=novo&search%5Bfilter_enum_condition%5D%5B1%5D=em_construcao&search%5Bdescription%5D=1&search%5Bsubregion_id%5D=195&search%5Border%5D=created_at_first%3Adesc&nrAdsPerPage=100`,
+  transform: function(body) {
+    $ = cheerio.load(body);
+  },
+  headers: {
+    "User-Agent":
+      "Mozilla/5.0 (X11; Linux x86_64)" +
+      "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36",
+    Accept:
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Language": "pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7,it;q=0.6,es;q=0.5"
+  }
+};
 
 function getData(client) {
-    var data = [];
+  var data = [];
 
-    request({
-        uri: `https://www.parsehub.com/api/v2/projects/tVdT9_R52ANa`,
-        method: 'GET',
-        gzip: true,
-        qs: {
-          api_key: "tTDbiWe13-na",
-          format: "json"
-        }
-    }, function(err, resp, body) {
-        var result = JSON.parse(body);
+  request(
+    {
+      uri: `https://www.parsehub.com/api/v2/projects/tVdT9_R52ANa`,
+      method: "GET",
+      gzip: true,
+      qs: {
+        api_key: "tTDbiWe13-na",
+        format: "json"
+      }
+    },
+    function(err, resp, body) {
+      var result = JSON.parse(body);
 
-        if(!result.last_run || new Date(result.last_run.start_time).setHours(23,59,59) < new Date(Date.now())) {
-            request({
-                uri: 'https://www.parsehub.com/api/v2/projects/tVdT9_R52ANa/run',
-                method: 'POST',
-                form: {
-                  api_key: "tTDbiWe13-na",
-                  send_email: "0"
-                }
-            }, function(err, resp, body) {
-                setTimeout(() => {
-                    request({
-                        uri: `https://www.parsehub.com/api/v2/runs/${JSON.parse(body).run_token}/data`,
-                        method: 'GET',
-                        qs: {
-                          api_key: "tTDbiWe13-na",
-                          format: "json"
-                        },
-                        gzip: true
-                    }, function(err, resp, body) {
-                        var result = JSON.parse(body);
-
-                        result.precos.forEach(element => {
-                            data.push({ price: element.value.replace(/\s/g, "").slice(0, -1) });
-                        });
-                    
-                        console.log("Scrap Done, " + data.length + " apartments found!");
-                    
-                        if(data.length == 0) {
-                            console.log(result);
-                        }
-                    
-                        saveData(data, client);
-                    });
-                }, 60000)
-            });
-        } else {
-            request({
-                uri: `https://www.parsehub.com/api/v2/runs/${result.last_run.run_token}/data`,
-                method: 'GET',
+      // if(!result.last_run || new Date(result.last_run.start_time).setHours(23,59,59) < new Date(Date.now())) {
+      request(
+        {
+          uri: "https://www.parsehub.com/api/v2/projects/tVdT9_R52ANa/run",
+          method: "POST",
+          form: {
+            api_key: "tTDbiWe13-na",
+            send_email: "0"
+          }
+        },
+        function(err, resp, body) {
+          setTimeout(() => {
+            request(
+              {
+                uri: `https://www.parsehub.com/api/v2/runs/${
+                  JSON.parse(body).run_token
+                }/data`,
+                method: "GET",
                 qs: {
                   api_key: "tTDbiWe13-na",
-                  format: "json"                  
+                  format: "json"
                 },
                 gzip: true
-            }, function(err, resp, body) {
+              },
+              function(err, resp, body) {
                 var result = JSON.parse(body);
 
-                result.precos.forEach(element => {
-                    data.push({ price: element.value.replace(/\s/g, "").slice(0, -1) });
+                result.selection1.forEach(element => {
+                  data.push({
+                    price: element.name.replace(/\s/g, "").replace("€/m²", "")
+                  });
                 });
-            
-                console.log("Scrap Done, " + data.length + " apartments found!");
-            
-                if(data.length == 0) {
-                    console.log(result);
+
+                console.log(
+                  "Scrap Done, " + data.length + " apartments found!"
+                );
+
+                if (data.length == 0) {
+                  console.log(result);
                 }
-            
+
                 saveData(data, client);
-            });
+              }
+            );
+          }, 60000);
         }
-    });
+      );
+      // } else {
+      //     request({
+      //         uri: `https://www.parsehub.com/api/v2/runs/${result.last_run.run_token}/data`,
+      //         method: 'GET',
+      //         qs: {
+      //           api_key: "tTDbiWe13-na",
+      //           format: "json"
+      //         },
+      //         gzip: true
+      //     }, function(err, resp, body) {
+      //         var result = JSON.parse(body);
+
+      //         result.precos.forEach(element => {
+      //             data.push({ price: element.value.replace(/\s/g, "").slice(0, -1) });
+      //         });
+
+      //         console.log("Scrap Done, " + data.length + " apartments found!");
+
+      //         if(data.length == 0) {
+      //             console.log(result);
+      //         }
+
+      //         saveData(data, client);
+      //     });
+      // }
+    }
+  );
 }
 
 function getOfferObject(element) {
-    var title = $(element).find('.offer-item-title').text();
-    var price = $(element).find('.offer-item-price').text().replace(/\s/g, "").slice(0, -1);
-    var pricePerM = $(element).find('.offer-item-price-per-m').text().replace(/\s/g, "").slice(0, -4);
+  var title = $(element)
+    .find(".offer-item-title")
+    .text();
+  var price = $(element)
+    .find(".offer-item-price")
+    .text()
+    .replace(/\s/g, "")
+    .slice(0, -1);
+  var pricePerM = $(element)
+    .find(".offer-item-price-per-m")
+    .text()
+    .replace(/\s/g, "")
+    .slice(0, -4);
 
-    return {
-        title,
-        price,
-        pricePerM
-    }
+  return {
+    title,
+    price,
+    pricePerM
+  };
 }
 
 function getSum(total, offer) {
-    const price = Math.round(offer.price);
-    
-    if(price) {
-        return total + price;
-    } else {
-        return total;
-    }
+  const price = Math.round(offer.price);
+
+  if (price) {
+    return total + price;
+  } else {
+    return total;
+  }
 }
 
 function saveData(data, client) {
-    
-    var totalPrice = data.reduce(getSum, 0);
-    var median = totalPrice / data.length;
-    var prices = data.map((a) => a.price);
-    var max = prices.reduce(function(priceA, priceB) {
-        if(!priceA) {
-            return priceB;
-        } else if(!priceB) {
-            return priceA;
-        } else {
-            return Math.max(priceA, priceB);
-        }
-    });
-    var min = prices.reduce(function(priceA, priceB) {        
-        if(!priceA) {
-            return priceB;
-        } else if(!priceB) {
-            return priceA;
-        } else {
-            return Math.min(priceA, priceB);
-        }
-    });
+  var totalPrice = data.reduce(getSum, 0);
+  var median = totalPrice / data.length;
+  var prices = data.map(a => a.price);
+  var max = prices.reduce(function(priceA, priceB) {
+    if (!priceA) {
+      return priceB;
+    } else if (!priceB) {
+      return priceA;
+    } else {
+      return Math.max(priceA, priceB);
+    }
+  });
+  var min = prices.reduce(function(priceA, priceB) {
+    if (!priceA) {
+      return priceB;
+    } else if (!priceB) {
+      return priceA;
+    } else {
+      return Math.min(priceA, priceB);
+    }
+  });
 
-    // for(var value in data) {
-        client.query("INSERT INTO values (median, max, min, date, source) VALUES ('" + median + "', '" + max + "', '" + min + "', '" + new Date(Date.now()) + "', 'imovirtual');", (err, res) => {
-            if (err) throw err;
-        });
-    
+  // for(var value in data) {
+  client.query(
+    "INSERT INTO values (median, max, min, date, source) VALUES ('" +
+      median +
+      "', '" +
+      max +
+      "', '" +
+      min +
+      "', '" +
+      new Date(Date.now()) +
+      "', 'imovirtual');",
+    (err, res) => {
+      if (err) throw err;
+    }
+  );
 
-    // client.end();
+  // client.end();
 
-    
+  // const collection = db
+  //     .defaults({ values: [] })
+  //     .get('values')
 
-
-
-
-    // const collection = db
-    //     .defaults({ values: [] })
-    //     .get('values')
-
-    
-
-    // const newValue = collection
-    //     .push({median, min, max, date: new Date(Date.now())})
-    //     .write();
+  // const newValue = collection
+  //     .push({median, min, max, date: new Date(Date.now())})
+  //     .write();
 }
 
 function getSavedData(startDate, endDate, client, callback) {
-    var data = [];
+  var data = [];
 
-    client.query("SELECT * FROM values;", (err, res) => {
-        if (err) throw err;
-        for (let row of res.rows) {
-            data.push({
-                median: row.median,
-                max: row.max,
-                min: row.min,
-                source: row.source,
-                date: row.date
-            })
-        }
-        // client.end();
+  client.query("SELECT * FROM values;", (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      data.push({
+        median: row.median,
+        max: row.max,
+        min: row.min,
+        source: row.source,
+        date: row.date
+      });
+    }
+    // client.end();
 
-        callback(data);
-    });
+    callback(data);
+  });
 
-
-    // return db.get('values')
-    //         .filter(function (v) {
-    //             return new Date(v.date) >= startDate && new Date(v.date) <= endDate
-    //         })
-    //         .value();
+  // return db.get('values')
+  //         .filter(function (v) {
+  //             return new Date(v.date) >= startDate && new Date(v.date) <= endDate
+  //         })
+  //         .value();
 }
 
 module.exports = {
-    getData,
-    getSavedData
-}
+  getData,
+  getSavedData
+};
--- index.js
+++ index.js
@@ -1,79 +1,92 @@
 var server_port = process.env.PORT || 8081;
-var server_ip_address = process.env.IP || '127.0.0.1';
+var server_ip_address = process.env.IP || "127.0.0.1";
 
-var express = require('express');
-var schedule = require('node-schedule');
+var express = require("express");
+var schedule = require("node-schedule");
 
-var imovirtualScraper =  require('./imovirtual-scrapper');
-var idealista = require('./idealista');
+var imovirtualScraper = require("./imovirtual-scrapper");
+var idealista = require("./idealista");
 
 var app = express();
 
-const { Client } = require('pg');
+const { Client } = require("pg");
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-//   connectionString: "postgres://postgres:password@localhost:5432/mylocaldb",
-  ssl: true,
+  //   connectionString: "postgres://postgres:password@localhost:5432/mylocaldb"
+  ssl: true
 });
 
 client.connect();
 
 // every 5 seconds
-var j = schedule.scheduleJob({hour: 16, minute: 00}, function(){
-    updateCurrentDay();
+var j = schedule.scheduleJob({ hour: 16, minute: 00 }, function() {
+  updateCurrentDay();
 });
 
-var i = schedule.scheduleJob({minute: 59}, function(){
-    console.log("I'm alive!");
+var i = schedule.scheduleJob({ minute: 59 }, function() {
+  console.log("I'm alive!");
 });
 
 function updateCurrentDay() {
-    var startDate = new Date(1543148511 * 1000);
-    var endDate = new Date(Date.now());
+  var startDate = new Date(1543148511 * 1000);
+  var endDate = new Date(Date.now());
 
-    var data = imovirtualScraper.getSavedData(startDate, endDate, client, getDataIfNeeded);
-
-    
+  var data = imovirtualScraper.getSavedData(
+    startDate,
+    endDate,
+    client,
+    getDataIfNeeded
+  );
 }
 
 function getDataIfNeeded(data) {
-    var currentDate = new Date(Date.now());
-    var latestDate = (new Date(data[data.length - 1].date)).setHours(23,59,59);
-    if (data.length == 0 || (latestDate < currentDate && currentDate.getHours() >= 15)) {
-        imovirtualScraper.getData(client);
-        idealista.getToken(client);
-    }
-} 
+  var currentDate = new Date(Date.now());
+  var latestDate = new Date(data[data.length - 1].date).setHours(23, 59, 59);
+  if (
+    data.length == 0 ||
+    (latestDate < currentDate && currentDate.getHours() >= 15)
+  ) {
+    imovirtualScraper.getData(client);
+    idealista.getToken(client);
+  }
+}
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-app.get('/', function(req, res) {
-    var startDate = new Date(parseInt(req.query.startDate) * 1000);
-    var endDate = new Date(parseInt(req.query.endDate) * 1000);
+app.get("/", function(req, res) {
+  var startDate = new Date(parseInt(req.query.startDate) * 1000);
+  var endDate = new Date(parseInt(req.query.endDate) * 1000);
 
-    imovirtualScraper.getSavedData(startDate, endDate, client, (result) => {
-        res.status(200).send(result);
-    });
-
+  imovirtualScraper.getSavedData(startDate, endDate, client, result => {
+    res.status(200).send(result);
+  });
 });
 
 app.listen(server_port, function() {
-    console.log('Server is running on server_ip_address ' + server_ip_address + ' and server_port:' + server_port);
+  console.log(
+    "Server is running on server_ip_address " +
+      server_ip_address +
+      " and server_port:" +
+      server_port
+  );
 
-    checkIfTableExistsAndCreate();
+  checkIfTableExistsAndCreate();
 
-    // idealista.getToken();
+  // idealista.getToken();
 
-    // idealista.getData();
+  // idealista.getData();
 
-    // imovirtualScraper.getData(client);
+  // imovirtualScraper.getData(client);
 
-    // getDataIfNeeded();
+  // getDataIfNeeded();
 });
 
 // client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
@@ -85,43 +98,48 @@ app.listen(server_port, function() {
 // });
 
 function checkIfTableExistsAndCreate() {
+  client.query("DROP TABLE values;", (err, res) => {
+    if (err) throw err;
+    // client.end();
 
-    // client.query("DROP TABLE values;", (err, res) => {
-    //     if (err) throw err;
-    //     // client.end();
+    console.log("Table Deleted!");
+  });
 
-    //     console.log("Table Deleted!");
-    // })
+  // client.query("DELETE FROM values WHERE date = 'Sat Dec 01 2018 17:02:09 GMT+0000 (Coordinated Universal Time)';", (err, res) => {
+  //     if (err) throw err;
+  //     // client.end();
 
-    // client.query("DELETE FROM values WHERE date = 'Sat Dec 01 2018 17:02:09 GMT+0000 (Coordinated Universal Time)';", (err, res) => {
-    //     if (err) throw err;
-    //     // client.end();
+  //     console.log("Row Deleted!");
+  // })
 
-    //     console.log("Row Deleted!");
-    // })
+  // client.query("DELETE FROM values WHERE date = 'Sat Dec 01 2018 17:02:19 GMT+0000 (Coordinated Universal Time)';", (err, res) => {
+  //     if (err) throw err;
+  //     // client.end();
 
-    // client.query("DELETE FROM values WHERE date = 'Sat Dec 01 2018 17:02:19 GMT+0000 (Coordinated Universal Time)';", (err, res) => {
-    //     if (err) throw err;
-    //     // client.end();
+  //     console.log("Row Deleted!");
+  // })
 
-    //     console.log("Row Deleted!");
-    // })
+  client.query(
+    "SELECT EXISTS ( SELECT 1 FROM information_schema.tables WHERE table_name = 'values' );",
+    (err, res) => {
+      if (err) throw err;
+      // client.end();
 
-    client.query("SELECT EXISTS ( SELECT 1 FROM information_schema.tables WHERE table_name = 'values' );", (err, res) => {
-        if (err) throw err;
-        // client.end();
+      if (!res.rows[0].exists) {
+        client.query(
+          "CREATE TABLE values (median float, max float, min float, date varchar(100), source varchar(10));",
+          (err, res) => {
+            if (err) throw err;
+            // client.end();
 
-        if(!res.rows[0].exists) {
-            client.query("CREATE TABLE values (median float, max float, min float, date varchar(100), source varchar(10));", (err, res) => {
-                if (err) throw err;
-                // client.end();
-        
-                console.log("Table Created!");
+            console.log("Table Created!");
 
-                updateCurrentDay();
-            })
-        } else {
             updateCurrentDay();
-        }
-    });
+          }
+        );
+      } else {
+        updateCurrentDay();
+      }
+    }
+  );
 }
